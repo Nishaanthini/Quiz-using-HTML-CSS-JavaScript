30f656119518d6219538fd523a07dfe5ded1eac1
Nishaanthini <141409805+Nishaanthini@users.noreply.github.com>
2024-07-22 17:39:36 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+       
+        var msg=document.getElementsByClassName("default");
+        function red(){
+               document.getElementsByClassName("red")[0].style.backgroundColor="red";
+        }
+        var wrong=document.getElementsByClassName("default");
+        function blue(){
+               document.getElementsByClassName("blue")[0].style.backgroundColor="red";
+        }
+        var wrong=document.getElementsByClassName("default");
+        function rose(){
+               document.getElementsByClassName("rose")[0].style.backgroundColor="red";
+        }
+        var wro=document.getElementsByClassName("ans");
+        function green(){
+               document.getElementsByClassName("green")[0].style.backgroundColor="green";
+        }
